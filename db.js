a03fe9706cfbe9dcaa20540fc6129da79352431b
vijay <vijayprajapati1410@gmail.com>
2025-02-14 11:02:40 +0530

    Connected MongoDB Atlas

--- db.js
+++ db.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();  // Load environment variables
 
 // Use environment variable for MongoDB URI
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce_server';
-
-mongoose.connect(mongoURI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB successfully'))
-.catch(err => console.error('MongoDB connection error:', err));
-
+.then(() => console.log('✅ Connected to MongoDB Atlas'))
+.catch(err => console.error('❌ MongoDB connection error:', err));
 module.exports = mongoose;
